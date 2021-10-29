78f2b84f8e436d4400ff363389ec7f883d2a062d
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-29 13:36:00 -0300

    Criar método GET

--- src/main/java/com/example/lead/Lead/LeadController.java
+++ src/main/java/com/example/lead/Lead/LeadController.java
@@ -1,8 +1,12 @@
 package com.example.lead.Lead;
 
+import com.example.lead.Lead.DTOs.LeadDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/lead")
@@ -10,5 +14,12 @@ public class LeadController {
 
     @Autowired
     LeadService leadService;
+
+    @GetMapping
+    public List<LeadDto> exibirLeads (){
+        return leadService.exibirLeads();
+    }
+
+
 
 }
--- src/main/java/com/example/lead/Lead/LeadService.java
+++ src/main/java/com/example/lead/Lead/LeadService.java
@@ -2,13 +2,21 @@ package com.example.lead.Lead;
 
 import com.example.lead.Lead.DTOs.LeadDto;
 import com.example.lead.Lead.DTOs.ProdutoDto;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class LeadService {
 
     List <LeadDto> leads = new ArrayList<>();
     List <ProdutoDto> produtos = new ArrayList<>();
+
+    public List <LeadDto> exibirLeads (){
+        return leads;
+    }
+
+
 
 }
