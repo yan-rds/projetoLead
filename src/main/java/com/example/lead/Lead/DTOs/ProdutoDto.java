ad70cc178431f152ddd74953221430a945cedcd3
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-29 11:39:45 -0300

    Moldar Classe ProdutoDto

--- src/main/java/com/example/lead/Lead/DTOs/ProdutoDto.java
+++ src/main/java/com/example/lead/Lead/DTOs/ProdutoDto.java
@@ -1,4 +1,30 @@
 package com.example.lead.Lead.DTOs;
 
 public class ProdutoDto {
+    private String nomeProduto;
+    private double valor;
+
+    public ProdutoDto() {
+    }
+
+    public ProdutoDto(String nomeProduto, double valor) {
+        this.nomeProduto = nomeProduto;
+        this.valor = valor;
+    }
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
 }
