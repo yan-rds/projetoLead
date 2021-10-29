0db4a3385252619fb97b89b04fb3c24ab270803f
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-29 11:38:43 -0300

    Moldar Classe LeadDto

--- src/main/java/com/example/lead/Lead/DTOs/LeadDto.java
+++ src/main/java/com/example/lead/Lead/DTOs/LeadDto.java
@@ -1,4 +1,52 @@
 package com.example.lead.Lead.DTOs;
 
+import java.util.List;
+
 public class LeadDto {
+    private String email;
+    private String nome;
+    private String telefone;
+    private List<ProdutoDto> produtos;
+
+    public LeadDto() {
+    }
+
+    public LeadDto(String email, String nome, String telefone, List<ProdutoDto> produtos) {
+        this.email = email;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.produtos = produtos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<ProdutoDto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<ProdutoDto> produtos) {
+        this.produtos = produtos;
+    }
 }
