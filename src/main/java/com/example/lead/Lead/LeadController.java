41559c9be1173fe5004d18ac5ca7a60bd7b29acb
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-29 11:48:50 -0300

    Criar listas no service

--- src/main/java/com/example/lead/Lead/LeadController.java
+++ src/main/java/com/example/lead/Lead/LeadController.java
@@ -10,4 +10,5 @@ public class LeadController {
 
     @Autowired
     LeadService leadService;
+
 }
--- src/main/java/com/example/lead/Lead/LeadService.java
+++ src/main/java/com/example/lead/Lead/LeadService.java
@@ -1,5 +1,14 @@
 package com.example.lead.Lead;
 
+import com.example.lead.Lead.DTOs.LeadDto;
+import com.example.lead.Lead.DTOs.ProdutoDto;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeadService {
+
+    List <LeadDto> leads = new ArrayList<>();
+    List <ProdutoDto> produtos = new ArrayList<>();
 
 }
